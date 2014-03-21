5272398e91d9a6069c7caf35684ad141ab2efddf
kosta-05 <kosta-05@kosta-05-PC>
2014-03-21 15:59:34 +0900

    mine

---
+++ NamooShop/src/com/namoosori/namooshop/service/logic/ProductServiceLogic.java
@@ -0,0 +1,35 @@
+package com.namoosori.namooshop.service.logic;
+
+import java.util.List;
+
+import com.namoosori.namooshop.data.ProductRepository;
+import com.namoosori.namooshop.domain.Product;
+import com.namoosori.namooshop.service.facade.ProductService;
+
+public class ProductServiceLogic implements ProductService {
+
+	private ProductRepository productRepo;
+	
+	public ProductServiceLogic() {
+		//
+		productRepo = ProductRepository.getInstance();
+	}
+	
+	@Override
+	public List<Product> getAllProducts() {
+		// 
+		return productRepo.findAllProducts();
+	}
+
+	@Override
+	public Product getProduct(int serialNo) {
+		// 
+		// 예외 발생 페이지 
+		// if (serialNo == 1) {
+		//	throw new RuntimeException("해당 상품의 재고가 없습니다.");
+		//}
+		
+		//
+		return productRepo.findProductByNo(serialNo);
+	}
+}
